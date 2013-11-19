ea75b4f1d2da090fcf787884c2c0fc9ce552895f
Ahmed Hani <ahani.fcis@gmail.com>
2013-11-19 19:12:28 +0200

    Quick Sort

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        public static int[] Arr = {2, 10, 0, 1, 0, 54, 12, 9, 50};
+        static void Main(string[] args)
+        {
+            quickSort Q = new quickSort();
+            Q.QSort(Arr, 0, Arr.Length - 1);
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine(Arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSort/quickSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class quickSort
+    {
+        public void QSort(int[] Arr, int LHS, int RHS)
+        {
+            int i = LHS;
+            int j = RHS;
+            int Pivot = Arr[(LHS + RHS) / 2];
+
+            while (i <= j)
+            {
+                while (Arr[i] < Pivot) i++;
+                while (Arr[j] > Pivot) j--;
+
+                if (i <= j)
+                {
+                    int Tmp = Arr[i];
+                    Arr[i] = Arr[j];
+                    Arr[j] = Tmp;
+                    i++;
+                    j--;
+                }
+            }
+            if (LHS < j) QSort(Arr, LHS, j);
+            if (RHS > i) QSort(Arr, i, RHS);
+        }
+    }
+}
